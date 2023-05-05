c865e64eee6b0de9b35d6272f308b87cf91ab17e
Ehsan Karegar Foroogh <ehsan.kf76@gmail.com>
2023-05-05 15:18:56 +0330

    Change add_marker & click; Added Feature: can't click one place twice

--- Tic-Tac-Toe.py
+++ Tic-Tac-Toe.py
@@ -13,6 +13,9 @@ tell_whose_turn = field.create_text(300, 20, fill="darkblue", font=font_turn,
 
 # Global variables for the game
 turn = 1
+game_field = [[0, 0, 0],
+              [0, 0, 0],
+              [0, 0, 0]]
 
 # Draw the lines
 field.create_line(200, 50, 200, 550)
@@ -26,34 +29,76 @@ field.create_line(50, 400, 550, 400)
 
 # For detecting mouse click for playing
 def click(event):
-    global turn
+    global turn, game_field
+    if turn%2 == 1:
+        text = "@"
+        color = "brown"
+    elif turn%2 == 0:
+        text = "#"
+        color = "darkcyan"
 
     # Check if the player clicked on ther proper field
     if event.x>60 and event.x<190:
         if event.y>60 and event.y<190:
-            add_marker(125, 125)
+            if game_field[0][0] != 0:
+                return
+            else:
+                game_field[0][0] = text
+                add_marker(125, 125, text, color)
         elif event.y>210 and event.y<390:
-            add_marker(125, 300)
+            if game_field[0][1] != 0:
+                return
+            else:
+                game_field[0][1] = text
+                add_marker(125, 300,text, color)
         elif event.y>410 and event.y<540:
-            add_marker(125, 475)
+            if game_field[0][2] != 0:
+                return
+            else:
+                game_field[0][2] = text
+                add_marker(125, 475, text, color)
         else:
             return
     elif event.x>210 and event.x<390:
         if event.y>60 and event.y<190:
-            add_marker(300, 125)
+            if game_field[1][0] != 0:
+                return
+            else:
+                game_field[1][0] = text
+                add_marker(300, 125, text, color)
         elif event.y>210 and event.y<390:
-            add_marker(300, 300)
+            if game_field[1][1] != 0:
+                return
+            else:
+                game_field[1][1] = text
+                add_marker(300, 300, text, color)
         elif event.y>410 and event.y<540:
-            add_marker(300, 475)
+            if game_field[1][2] != 0:
+                return
+            else:
+                game_field[1][2] = text
+                add_marker(300, 475, text, color)
         else:
             return
     elif event.x>410 and event.x<540:
         if event.y>60 and event.y<190:
-            add_marker(475, 125)
+            if game_field[2][0] != 0:
+                return
+            else:
+                game_field[2][0] = text
+                add_marker(475, 125, text, color)
         elif event.y>210 and event.y<390:
-            add_marker(475, 300)
+            if game_field[2][1] != 0:
+                return
+            else:
+                game_field[2][1] = text
+                add_marker(475, 300, text, color)
         elif event.y>410 and event.y<540:
-            add_marker(475, 475)
+            if game_field[2][2] != 0:
+                return
+            else:
+                game_field[2][2] = text
+                add_marker(475, 475, text, color)
         else:
             return
     else:
@@ -64,7 +109,7 @@ def click(event):
 
 # For telling the players whose turn it is
 def change_turn():
-    global turn, tell_whose_turn
+    global turn, tell_whose_turn, font_turn
     if turn%2 == 0:
         field.delete(tell_whose_turn)
         tell_whose_turn = field.create_text(300, 20, fill="darkblue", font=font_turn,
@@ -76,14 +121,10 @@ def change_turn():
                                             text="@ Turn to click")
 
 # For adding Signs in the game field
-def add_marker(x, y):
-    global turn
-    if turn%2==1:
-        field.create_text(x, y, fill="brown", font=font_marker,
-                          text="@")
-    elif turn%2==0:
-        field.create_text(x, y, fill="darkcyan", font=font_marker,
-                          text="#")
+def add_marker(x, y, txt, color):
+    global font_marker
+    field.create_text(x, y, fill=color, font=font_marker,
+                      text=txt)
 
 # For checking if a player has won the game
 def check_win():
